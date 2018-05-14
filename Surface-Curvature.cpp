75b2c0e53a31d78f5dd6190be981949b4f5fd940
Dimitrisgian1 <30804541+Dimitrisgian1@users.noreply.github.com>
2018-05-15 01:15:44 +0300

    Completed->Triangulation + Curvature

--- Surface-Curvature.cpp
+++ Surface-Curvature.cpp
@@ -1,9 +1,10 @@
 #include "SceneMesh3D.h"
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
-// My global mesh
-//Mesh my_mesh;
+//My global mesh
+//vvr::Mesh my_mesh;
 
 Mesh3DScene::Mesh3DScene()
 {
@@ -14,11 +15,10 @@ Mesh3DScene::Mesh3DScene()
     m_bg_col = vvr::Colour("768E77");
     m_obj_col = vvr::Colour("454545");
     const std::string objDir = vvr::getBasePath() + "resources/obj/";
-    const std::string objFile = objDir + "suzanne.obj";
+    const std::string objFile = objDir + "dragon_low_low.obj";
     m_model_original = vvr::Mesh(objFile);
     reset();
 }
-
 void Mesh3DScene::reset()
 {
     Scene::reset();
@@ -29,9 +29,8 @@ void Mesh3DScene::reset()
     m_style_flag |= FLAG_SHOW_WIRE;
     m_style_flag |= FLAG_SHOW_AXES;
     m_style_flag |= FLAG_SHOW_AABB;
-    //m_style_flag |= FLAG_SHOW_PLANE;
+	//m_style_flag |= FLAG_SHOW_TASK1; 
 }
-
 void Mesh3DScene::resize()
 {
     //! By Making `first_pass` static and initializing it to true,
@@ -48,17 +47,6 @@ void Mesh3DScene::resize()
         first_pass = false;
     }
 }
-
-void Mesh3DScene::Tasks()
-{
-	FindCenterMass(m_model.getVertices(), m_center_mass);
-
-	FindAABB(m_model.getVertices(), m_aabb);
-
-	std::vector<int> rand_tri_indices;
-	ChooseRandTri(m_model.getTriangles(), rand_tri_indices);
-}
-
 void Mesh3DScene::keyEvent(unsigned char key, bool up, int modif)
 {
     Scene::keyEvent(key, up, modif);
@@ -72,24 +60,32 @@ void Mesh3DScene::keyEvent(unsigned char key, bool up, int modif)
     case 'a': m_style_flag ^= FLAG_SHOW_AXES; break;
     case 'p': m_style_flag ^= FLAG_SHOW_PLANE; break;
     case 'b': m_style_flag ^= FLAG_SHOW_AABB; break;
+	case '1': m_style_flag ^= FLAG_SHOW_TASK1; Task1(); break;
     }
 }
 
+void Mesh3DScene::Tasks()
+{
+	if (m_style_flag & FLAG_SHOW_TASK1) Task1();
+}
 
 void Mesh3DScene::draw()
 {
+	//vvr::Triangle3D t3d(
+	//	tri.v1.x, tri.v1.y, tri.v1.z,
+	//	triv2.x, tri.v2.y, tri.v2.z,
+	//	tri.v3.x, tri.v3.y, tri.v3.z,
+	//	Colour::green);
+	//t3d.draw();
+	//vvr::Point3D(opp_ver.x, opp_ver.y, opp_ver.z, vvr::Colour::red).draw();
 
 	if (m_style_flag & FLAG_SHOW_SOLID) m_model.draw(m_obj_col, vvr::SOLID);
 	if (m_style_flag & FLAG_SHOW_WIRE) m_model.draw(vvr::Colour::black, vvr::WIRE);
 	if (m_style_flag & FLAG_SHOW_NORMALS) m_model.draw(vvr::Colour::black, vvr::NORMALS);
 	if (m_style_flag & FLAG_SHOW_AXES) m_model.draw(vvr::Colour::black, vvr::AXES);
 
-	// Draw my new green split mesh, solid and wireframe
-	//my_mesh.draw(Colour::green, SOLID);
-	//my_mesh.draw(Colour::green, WIRE);
-
 	//! Draw center mass
-	vvr::Point3D(m_center_mass.x, m_center_mass.y, m_center_mass.z, vvr::Colour::red).draw();
+	//vvr::Point3D(m_center_mass.x, m_center_mass.y, m_center_mass.z, vvr::Colour::red).draw();
 
 	//! Draw AABB
 	if (m_style_flag & FLAG_SHOW_AABB) {
@@ -97,7 +93,8 @@ void Mesh3DScene::draw()
 		m_aabb.setTransparency(1);
 		m_aabb.draw();
 	}
-	
+	//for (int i = 0; i < TriNormals.size(); i++)
+	//	vvr::Point3D(TriNormals[i].x*100, TriNormals[i].y * 100, TriNormals[i].z * 100, vvr::Colour::black).draw();
 }
 
 int main(int argc, char* argv[])
@@ -116,58 +113,103 @@ int main(int argc, char* argv[])
     }
 }
 
-void FindCenterMass(std::vector<vec> &vertices, vec &cm)
+void Mesh3DScene::Task1()
 {
-	cm.x = 0;
-	cm.y = 0;
-	cm.z = 0;
-	for (int i = 0; i < vertices.size(); i++)
-	{
-		cm.x += vertices[i].x;
-		cm.y += vertices[i].y;
-		cm.z += vertices[i].z;
-	}
+	std::vector<int> rand_tri_indices;
+	std::vector<vvr::Triangle> &triangles = m_model.getTriangles();
+	std::vector<vec>		   &verts = m_model.getVertices();
 
-	cm.x /= vertices.size();
-	cm.y /= vertices.size();
-	cm.z /= vertices.size();
+	std::vector<vvr::Triangle> NeighbourTris;
+
+	StoreNeighbourTris(triangles, verts[0], NeighbourTris);
+	Curvature(verts[0], NeighbourTris);
+	FixManyTris(triangles, rand_tri_indices, verts);
+	m_model.update();
 }
 
-void FindAABB(std::vector<vec> &vertices, vvr::Box3D &aabb)
+void FixManyTris(std::vector<vvr::Triangle> &triangles, std::vector<int> &rand_tri_indices, std::vector<vec> &verts)
 {
-	vvr::Point3D V_min(1000, 1000, 1000), V_max(0, 0, 0);
-
-	for (int i = 0; i < vertices.size(); i++)
-	{
-		V_min.x = (vertices[i].x < V_min.x) ? vertices[i].x : V_min.x;
-		V_min.y = (vertices[i].y < V_min.y) ? vertices[i].y : V_min.y;
-		V_min.z = (vertices[i].z < V_min.z) ? vertices[i].z : V_min.z;
-
-		V_max.x = (vertices[i].x > V_max.x) ? vertices[i].x : V_max.x;
-		V_max.y = (vertices[i].y > V_max.y) ? vertices[i].y : V_max.y;
-		V_max.z = (vertices[i].z > V_max.z) ? vertices[i].z : V_max.z;
-	}
-
-	aabb.x1 = V_min.x;
-	aabb.y1 = V_min.y;
-	aabb.z1 = V_min.z;
-	aabb.x2 = V_max.x;
-	aabb.y2 = V_max.y;
-	aabb.z2 = V_max.z;
+	ChooseRandTris(triangles, rand_tri_indices);
+	FixRandTri(triangles, rand_tri_indices, verts);
 }
 
-void ChooseRandTri(std::vector<vvr::Triangle> &triangles, std::vector<int> &rand_tri_indices)
+void ChooseRandTris(std::vector<vvr::Triangle> &triangles, std::vector<int> &rand_tri_indices)
 {
-	for (int i = 0; i < triangles.size() / 10; i++)
+	for (int i = 0; i < 100; i++)///////////////////////////o ari8mos twn epanalhpsewn ka8orizei ton ari8mo twn tuxaiwn trigwnwn pou epilegw
 	{
 		//srand(i);
-		int rand_index = rand() % triangles.size();
+		int rand_index = rand() % triangles.size() - 1;
 		rand_tri_indices.push_back(rand_index);
-		//triangles.erase(triangles.begin() + rand_tri_indices[i]);
 	}
 }
 
-vec ChooseRandTriVert(vvr::Triangle triangle)
+void FixRandTri(std::vector<vvr::Triangle> &triangles, std::vector<int> &rand_tri_indices, std::vector<vec> &verts)
+{
+	int counter = 0;
+	for (int i : rand_tri_indices)
+	{
+		unsigned tri_adj_index;
+		vec P1, P2, P3, opp_ver;
+		bool flag = 0;
+
+		if (i > triangles.size()) i = rand() % triangles.size() - 1;
+
+		P1 = ChooseRandTriVert(triangles[i], P2, P3);
+		FindAdjacentTriangle(triangles, P1, P2, P3, tri_adj_index, opp_ver);
+
+		//float AnglesBef[6] = { angle(P1, P2, P3),	   angle(P2, P1, P3),	   angle(P3, P1, P2), 
+		//					   angle(opp_ver, P2, P3), angle(P2, P3, opp_ver), angle(P3, P2, opp_ver) };
+
+		//float AnglesAft[6] = { angle(P1, P2, opp_ver), angle(opp_ver, P2, P1), angle(P2, P1, opp_ver),
+		//					   angle(P1, opp_ver, P3), angle(opp_ver, P1, P3), angle(P3, P1, opp_ver) };
+
+		//int counter;
+
+		//for (int k = 0; k < 6; k++)
+		//{
+		//	counter = 0;
+		//	for (int j = 0; j < 6; j++)
+		//		if (AnglesBef[k] < AnglesAft[j]) counter++;
+		//	if (counter == 6) flag = 1;
+		//}
+
+		if (angle(P1, P2, P3) + angle(opp_ver, P2, P3) > 200 * pi / 180) flag = 1;
+		if (flag)
+		{
+			counter++;
+			std::vector<vec> verts1 = { P1, P2, opp_ver };
+			std::vector<vec> verts2 = { P1, P3, opp_ver };
+
+			for (int l = 0; l < 3; l++)
+				verts.push_back((verts1[l]));
+
+			unsigned vi = verts.size() - 3;
+
+			triangles.push_back(vvr::Triangle(&verts, vi, vi + 1, vi + 2));
+
+			for (int l = 0; l < 3; l++)
+				verts.push_back((verts2[l]));
+
+			vi = verts.size() - 3;
+
+			triangles.push_back(vvr::Triangle(&verts, vi, vi + 1, vi + 2));
+
+			if (i > tri_adj_index)
+			{
+				triangles.erase(triangles.begin() + i);
+				triangles.erase(triangles.begin() + tri_adj_index);
+			}
+			else
+			{
+				triangles.erase(triangles.begin() + tri_adj_index);
+				triangles.erase(triangles.begin() + i);
+			}
+		}
+	}
+	std::cout << counter << std::endl;
+}
+
+vec ChooseRandTriVert(vvr::Triangle &triangle, vec &p2, vec &p3)
 {
 	std::vector<vec> vert;
 	vert.push_back((triangle).v1());
@@ -176,13 +218,288 @@ vec ChooseRandTriVert(vvr::Triangle triangle)
 
 	//srand(time(NULL));
 	int rand_vert = rand() % 3;
+
+	if (rand_vert == 0)
+	{
+		vec v2 = vert[rand_vert + 1];
+		vec v3 = vert[rand_vert + 2];
+
+		p2.Set(v2.x, v2.y, v2.z);
+		p3.Set(v3.x, v3.y, v3.z);
+	}
+
+	if (rand_vert == 1)
+	{
+		vec v2 = vert[rand_vert - 1];
+		vec v3 = vert[rand_vert + 1];
+
+		p2.Set(v2.x, v2.y, v2.z);
+		p3.Set(v3.x, v3.y, v3.z);
+	}
+
+	if (rand_vert == 2)
+	{
+		vec v2 = vert[rand_vert - 2];
+		vec v3 = vert[rand_vert - 1];
+
+		p2.Set(v2.x, v2.y, v2.z);
+		p3.Set(v3.x, v3.y, v3.z);
+	}
 	return vert[rand_vert];
 }
 
-void FixRandTri(std::vector<vvr::Triangle> &triangles, std::vector<int> &rand_tri_indices)
+void FindAdjacentTriangle(std::vector<vvr::Triangle> &triangles, vec p1, vec p2, vec p3, unsigned &tri_adj_index, vec &opp_ver)
 {
-	for (int i : rand_tri_indices)
+	for (int i = 0; i < triangles.size(); i++)		//oles oi periptwseis korufwn i = shmeiwn j
 	{
-		vec P1 = ChooseRandTriVert(triangles[i]);
+		vec v1 = triangles[i].v1();
+		vec v2 = triangles[i].v2();
+		vec v3 = triangles[i].v3();
+
+		if (p1.Equals(v1) || p1.Equals(v2) || p1.Equals(v3)) continue;
+
+		if (p2.Equals(v1))		//v1 -> p2
+		{
+			if (p3.Equals(v2))	//v2 -> p3
+			{
+				opp_ver.Set(v3.x, v3.y, v3.z);
+				tri_adj_index = i;
+				return;
+			}
+
+			if (p3.Equals(v3))	//v3 -> p3
+			{
+				opp_ver.Set(v2.x, v2.y, v2.z);
+				tri_adj_index = i;
+				return;
+			}
+		}
+
+		if (p2.Equals(v2))		//v2 -> p2
+		{
+			if (p3.Equals(v1))	//v1 -> p3
+			{
+				opp_ver.Set(v3.x, v3.y, v3.z);
+				tri_adj_index = i;
+				return;
+			}
+
+			if (p3.Equals(v3))	//v3 -> p3
+			{
+				opp_ver.Set(v1.x, v1.y, v1.z);
+				tri_adj_index = i;
+				return;
+			}
+		}
+
+		if (p2.Equals(v3))		//v3 -> p2
+		{
+			if (p3.Equals(v1))	//v1 -> p3
+			{
+				opp_ver.Set(v2.x, v2.y, v2.z);
+				tri_adj_index = i;
+				return;
+			}
+
+			if (p3.Equals(v2))	//v2 -> p3
+			{
+				opp_ver.Set(v1.x, v1.y, v1.z);
+				tri_adj_index = i;
+				return;
+			}
+		}
 	}
+	return;
+}
+
+//returns the inner angle P2P1P3
+float angle(vec P1, vec P2, vec P3)
+{
+	float dx21 = P2.x - P1.x;
+	float dx31 = P3.x - P1.x;
+	float dy21 = P2.y - P1.y;
+	float dy31 = P3.y - P1.y;
+	float m12 = sqrt(dx21*dx21 + dy21 * dy21);
+	float m13 = sqrt(dx31*dx31 + dy31 * dy31);
+	float theta = acos((dx21*dx31 + dy21 * dy31) / (m12 * m13));
+	return theta;
+}
+
+//Finds all neigbouring vertices to one vertex and stores theim in NeigbourVerts 
+void StoreNeighbourVertices(std::vector<vvr::Triangle> &triangles, vec P, std::vector<math::vec> &NeigbourVerts)
+{
+	for (int i = 0; i < triangles.size(); i++)
+	{
+		vec v1 = triangles[i].v1();
+		vec v2 = triangles[i].v2();
+		vec v3 = triangles[i].v3();
+
+		if (P.Equals(v1))
+		{
+			NeigbourVerts.push_back(v2);
+			NeigbourVerts.push_back(v3);
+		}
+		if (P.Equals(v2))
+		{
+			NeigbourVerts.push_back(v1);
+			NeigbourVerts.push_back(v3);
+		}
+		if (P.Equals(v3))
+		{
+			NeigbourVerts.push_back(v1);
+			NeigbourVerts.push_back(v2);
+		}
+	}
+}
+
+//Returns the normal of a vertex
+vec FindVertNormal(std::vector<vvr::Triangle> &triangles, vec P)
+{
+	std::vector<vvr::Triangle> NeighbourTris;
+	StoreNeighbourTris(triangles, P, NeighbourTris);
+
+	vec normal(0, 0, 0);
+	for (int i = 0; i < NeighbourTris.size(); i++)
+	{
+		vec P1 = triangles[i].v1();
+		vec P2 = triangles[i].v2();
+		vec P3 = triangles[i].v3();
+		math::Triangle T(P1, P2, P3);
+		normal += T.Area() * T.NormalCCW();//prose3e to auto sto laplacian mesh smoothing
+	}
+	normal.x = normal.x / normal.Length();
+	normal.y = normal.y / normal.Length();
+	normal.z = normal.z / normal.Length();
+
+	return normal;
+}
+
+//this is how StoreNeighbourVertices is used, You cannot have the indices to the neighbours this way
+void FindNeighbourVertices(std::vector<vvr::Triangle> &triangles, vec P)
+{
+	std::vector<math::vec> NeighbourVerts;
+	StoreNeighbourVertices(triangles, P, NeighbourVerts);
+}
+
+//Finds all neigbouring triangles to one vertex and stores them in NeigbourTris 
+void StoreNeighbourTris(std::vector<vvr::Triangle> &triangles, vec P, std::vector<vvr::Triangle> &NeigbourTris)
+{
+	for (int i = 0; i < triangles.size(); i++)
+	{
+		vec v1 = triangles[i].v1();
+		vec v2 = triangles[i].v2();
+		vec v3 = triangles[i].v3();
+
+		if (P.Equals(v1) || P.Equals(v2) || P.Equals(v3))
+			NeigbourTris.push_back(triangles[i]);
+	}
+}
+
+void Curvature(vec P, std::vector<vvr::Triangle> &NeigbourTris)
+{
+	float Area = 0;
+	vec MeanCurv(0,0,0);
+	float GaussCurv = 2*pi;
+
+	std::vector<vvr::Triangle> Pairs;
+	std::vector<int> TriVertIsP;
+	for (int i = 0; i < NeigbourTris.size(); i++)
+	{
+		vec v1 = NeigbourTris[i].v1();
+		vec v2 = NeigbourTris[i].v2();
+		vec v3 = NeigbourTris[i].v3();
+		if (P.Equals(v1)) TriVertIsP.push_back(0);
+		if (P.Equals(v2)) TriVertIsP.push_back(1);
+		if (P.Equals(v3)) TriVertIsP.push_back(2);
+	}
+
+	for (int i = 0; i < NeigbourTris.size(); i++)
+	{
+		{
+			for (int j = 0; j < NeigbourTris.size(); j++)
+			{
+				if (j == i) continue;
+				std::vector<vec> vi = { NeigbourTris[i].v1(), NeigbourTris[i].v2(), NeigbourTris[i].v3() };
+				std::vector<vec> vj = { NeigbourTris[j].v1(), NeigbourTris[j].v2(), NeigbourTris[j].v3() };
+
+				vec vk = vi[(TriVertIsP[i] + 1) % 3];	 //second common point of triangle i
+
+				if (vk.Equals(vj[(TriVertIsP[j] + 1) % 3]))//second common point of triangle j
+				{
+					vec vA = vi[(TriVertIsP[i] + 2) % 3];//third (uncommon) point of triangle i
+					vec vB = vi[TriVertIsP[i]];			 //first common point of triangle i
+					vec vC = vj[(TriVertIsP[j] + 2) % 3];//third (uncommon) point of triangle j
+
+					float a = angle(vA, vB, vk);
+					float b = angle(vC, vB, vk);
+					Area += (1 / tan(a)) + (1 / tan(b));
+					Area *= (vB.DistanceSq(vk)) / 8;
+					
+					MeanCurv.x += (1 / tan(a)) + (1 / tan(b)) * (vk.x - P.x);
+					MeanCurv.y += (1 / tan(a)) + (1 / tan(b)) * (vk.y - P.y);
+					MeanCurv.z += (1 / tan(a)) + (1 / tan(b)) * (vk.z - P.z);
+
+					GaussCurv -= angle(vB, vk, vA);
+				}
+
+				if (vk.Equals(vj[(TriVertIsP[j] + 2) % 3]))//second common point of triangle j
+				{
+					vec vA = vi[(TriVertIsP[i] + 2) % 3];//third (uncommon) point of triangle i
+					vec vB = vi[TriVertIsP[i]];			 //first common point of triangle i
+					vec vC = vj[(TriVertIsP[j] + 1) % 3];//third (uncommon) point of triangle j
+
+					float a = angle(vA, vB, vk);
+					float b = angle(vC, vB, vk);
+					Area += (1 / tan(a)) + (1 / tan(b));
+					Area *= (vB.DistanceSq(vk)) / 8;
+
+					MeanCurv.x += (1 / tan(a)) + (1 / tan(b)) * (vk.x - P.x);
+					MeanCurv.y += (1 / tan(a)) + (1 / tan(b)) * (vk.y - P.y);
+					MeanCurv.z += (1 / tan(a)) + (1 / tan(b)) * (vk.z - P.z);
+
+					GaussCurv -= angle(vB, vk, vA);
+				}
+
+				vk = vi[(TriVertIsP[i] + 2) % 3];	 //second common point of triangle i
+
+				if (vk.Equals(vj[(TriVertIsP[j] + 1) % 3]))//second common point of triangle j
+				{
+					vec vA = vi[(TriVertIsP[i] + 1) % 3];//third (uncommon) point of triangle i
+					vec vB = vi[TriVertIsP[i]];			 //first common point of triangle i
+					vec vC = vj[(TriVertIsP[j] + 2) % 3];//third (uncommon) point of triangle j
+
+					float a = angle(vA, vB, vk);
+					float b = angle(vC, vB, vk);
+					Area += (1 / tan(a)) + (1 / tan(b));
+					Area *= (vB.DistanceSq(vk)) / 8;
+
+					MeanCurv.x += (1 / tan(a)) + (1 / tan(b)) * (vk.x - P.x);
+					MeanCurv.y += (1 / tan(a)) + (1 / tan(b)) * (vk.y - P.y);
+					MeanCurv.z += (1 / tan(a)) + (1 / tan(b)) * (vk.z - P.z);
+
+					GaussCurv -= angle(vB, vk, vA);
+				}
+
+				if (vk.Equals(vj[(TriVertIsP[j] + 2) % 3]))//second common point of triangle j
+				{
+					vec vA = vi[(TriVertIsP[i] + 2) % 3];//third (uncommon) point of triangle i
+					vec vB = vi[TriVertIsP[i]];			 //first common point of triangle i
+					vec vC = vj[(TriVertIsP[j] + 1) % 3];//third (uncommon) point of triangle j
+
+					float a = angle(vA, vB, vk);
+					float b = angle(vC, vB, vk);
+					Area += (1 / tan(a)) + (1 / tan(b));
+					Area *= (vB.DistanceSq(vk)) / 8;
+
+					MeanCurv.x += (1 / tan(a)) + (1 / tan(b)) * (vk.x - P.x);
+					MeanCurv.y += (1 / tan(a)) + (1 / tan(b)) * (vk.y - P.y);
+					MeanCurv.z += (1 / tan(a)) + (1 / tan(b)) * (vk.z - P.z);
+
+					GaussCurv -= angle(vB, vk, vA);
+				}
+			}
+		}
+	}
+	MeanCurv /= (4 * Area);
+	GaussCurv /= Area;
 }
